cafab0323959212c2d6d66c21f327c64bd088117
twtw <61638296+erasersleeve@users.noreply.github.com>
2020-07-28 12:16:40 -0400

    route refinement, notes for other thoughts.

--- models/workout.js
+++ models/workout.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true
+  day: {
+    type: Date,
+    default: Date.now
   },
-  day: new Date().setDate(new Date()),
   exercises: [
     {
       type: Schema.Types.ObjectId,
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+
+
 
 const PORT = process.env.PORT || 8080
 
 const app = express();
+app.use(logger("dev"));
+const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,14 +23,81 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 
+//create new workout form
 app.get("/exercise", function (req, res) {
-    res.sendFile(__dirname + "/public/exercise.html")
+    res.sendFile(path.join(__dirname + "/public/exercise.html"))
 });
 
+//work out tracker
 app.get("/stats", (req, res) => {
-    res.sendFile(__dirname + "/public/stats.html")
+    res.sendFile(path.join(__dirname + "/public/stats.html"))
 });
 
+//workouts
+// fetch("/api/workouts/range")
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     populateChart(data);
+//   });
+app.get("/exercises", (req, res) => {
+    db.Exercise.find({})
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    // .populate("exercises")
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    }) 
+})
+
+
+app.post("/api/workouts", (req, res) => {
+    //{day: Date.now() }   ??
+    db.Workout.create()
+    .then (dbWorkout => { 
+        console.log(dbWorkout);
+        location.search = "?id=" + workout_id;
+    })
+
+    //catch {message} 
+    .catch(err => {
+        res.json(err);
+    })
+})
+
+
+//Add to an Existing Workout
+app.put("/api/workouts/:id", (req, res) => {
+    db.Exercise.create(req.body)
+        .then(({ _id }) => db.Workout.findOneAndUpdate(
+            { 
+                _id: req.params.id 
+            }, { 
+                $push: { exercises: _id } 
+            }, { 
+                new: true 
+            })
+            )
+        .then(dbWorkout => {
+            console.log(req.params);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+})
 
 
 app.listen(PORT, () => {
